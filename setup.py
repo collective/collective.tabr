beba92a45bcb9bbcae299ab0722eb73247abb7dc
dunlapm <dunlapm@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-06-24 17:32:33 +0000

    initial import

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.tabr',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Michael Dunlap',
+      author_email='dunlapm@uw.edu',
+      url='http://svn.plone.org/svn/collective/collective.tabr/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.jquerytools'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      #paster_plugins = ["ZopeSkel"],
+      )
